e1b19ff554831e10e03a7bdf1f015e847271716f
luca <luca@m47rix.com>
2022-04-19 18:10:01 +0900

    220419 18:09

--- DK_library_change_practice/practice.py
+++ DK_library_change_practice/practice.py
@@ -1,99 +1,66 @@
 
+# class MyStatus: 
+#     def __init__(self,age,name,height,weight): 
+#         # self.age = age 
+#         # self.name = name
+#         # self.height = height
+#         # self.weight = weight 
+#     def print_name(self):
+#         print(self.name)
+#     def print_age(self):
+#         print(self.age)
+#     def print_height(self):
+#         print(self.height)
+#     def print_weight(self):
+#         print(self.weight)
 
-# import cv2
+# a = MyStatus(34,"yamada",170,78)
 
-
-
-# def func1(num):
-#     return cv2.VideoCapture(num)
-
-# def func2(cam, num):
-#     while cam.isOpened():
-#         ret, img = cam.read()
-#         if not ret:
-#             break
-
-#         img = cv2.flip(img, num) # mirror image
-
-#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-#         cv2.imshow('title', img)     # img를 실시간으로 출력 / 제목은 "arg"
-#         if cv2.waitKey(1) == ord('q'):
-#             break
+# a.print_age()
 
 
 
 
 
 
+class Camera():
+
+    def __init__(self):
+        
+        pass
+        
+    def is_opened():
+        # self.camera_open = self.camera.isOpend()
+        # return self.camera_open
+        
+        return camera.isOpened()
 
 
 
 
 
+import cv2
 
+cap = cv2.VideoCapture(0)    # 웹캠 키기
 
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 300)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 400)
 
+while cap.isOpened():
+    ret, img = cap.read()
+    if not ret:
+        break
 
+    img = cv2.flip(img, 1) # mirror image
 
-############################## 변경 전 코드 #############################
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-
-
-# cap = cv2.VideoCapture(0)    # 웹캠 키기
-# cap = func1(0)
-
-
-# while cap.isOpened():
-#     ret, img = cap.read()
-#     if not ret:
-#         break
-
-#     img = cv2.flip(img, 1) # mirror image
-
-#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-# func2(1)
-
-#     cv2.imshow('title', img)     # img를 실시간으로 출력 / 제목은 "arg"
-#     if cv2.waitKey(1) == ord('q'):
-#         break
+    cv2.imshow('title', img)     # img를 실시간으로 출력 / 제목은 "arg"
+    if cv2.waitKey(1) == ord('q'):
+        break
 
 # 캠 끄기 cam.end()
-# cap.release()
-
-
-
-
-
-# from dynamikontrol import Module, cam
-
-# cam = cam.start("cam_index = 0 or 1")             # size 
-
-# while cam.open():
-#     img = cam.to_img(  "mirror_mode = 0 or 1" )
-#     cam.show( img , "cam_name", "quit_string" )
-
-# cam.end()
-
-
-
-from dynamikontrol import Module
-
-module = Module()
-
-module.motor.angle(0)
-
-module.disconnect()
-
-
-
-
-
-
-
-
-
+cap.release()
 
 
 
