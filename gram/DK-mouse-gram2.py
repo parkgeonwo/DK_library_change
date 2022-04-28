77e31bb7bdaffa4b35ed83725d47569d009dd635
Geonwoo Park <parkkeonwo@naver.com>
2022-04-28 22:53:06 +0900

    220428 22:53

--- gram/DK-mouse-gram2.py
+++ gram/DK-mouse-gram2.py
@@ -15,6 +15,7 @@ mp_face_mesh = mp.solutions.face_mesh
 # For webcam input:
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 cap = cv2.VideoCapture(0)
+
 with mp_face_mesh.FaceMesh(
     max_num_faces=1,
     refine_landmarks=True,
