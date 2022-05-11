8d6ed327ec04aa26d018ff0d154804dfab6db37b
Geonwoo Park <parkkeonwo@naver.com>
2022-05-11 22:15:40 +0900

    220511 10:15

--- DK_library_change_practice/main.py
+++ DK_library_change_practice/main.py
@@ -6,12 +6,20 @@ camera = Camera()
 
 while camera.is_opened():
     frame = camera.get_frame()
-    face = camera.detect_face(frame, draw_face=False, draw_lips=False )
+    face = camera.detect_face(frame, draw_face=False,
+                        draw_lips=False, write_direction=False)
     if face:
-        if face.eyes.is_look_left():
+        if face.eyes.is_look_left() or face.eyes.is_look_right():
             module.motor.angle(-50)
             module.motor.angle(50, period = 2)
     camera.show(frame)
+
+
+
+
+
+
+
 
 
 
