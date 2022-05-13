d574e29530a75a20f12a208a51282f14acfb31ef
luca <luca@m47rix.com>
2022-05-13 18:54:28 +0900

    220513 18:54

--- DK_library_change_practice/practice.py
+++ DK_library_change_practice/practice.py
@@ -23,10 +23,16 @@ camera = Camera()
 while camera.is_opened():
     frame = camera.get_frame()
 
-    hand = camera.detect_hand(frame)
+    hand = camera.detect_hand(frame,write_shape=True, write_hand_distance=True)
     if hand:
-        distance = hand.find_finger_distance("index","middle")
-        print(distance)
+        print(hand.fingers.is_up())
+
+    # face = camera.detect_face(frame)
+
+    # if face:
+    #     print(face.landmark_list[1])
+    #     if face.direction == "up":
+    #         print("wow")
 
     camera.show(frame)
 
